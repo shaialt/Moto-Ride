8d1421859576e06c32ab600975ed9c0b39156d01
Shai Levi Altshtein <shailevi2004@gmail.com>
2025-05-25 15:13:32 +0300

    Navigation manu

--- moto ride.js
+++ moto ride.js
@@ -1,3 +1,40 @@
+// סגירת תפריט ניווט ראשי
+let closeMenue = document.querySelector('.navigation_menu_close_button');
+closeMenue.addEventListener('click', closeMenueFunction);
+
+function closeMenueFunction (event){
+    let navigation_menu = document.querySelector('.navigation_menu');
+    navigation_menu.classList.add('navigation_menu_close_click');
+    navigation_menu.classList.remove('navigation_menu_open_click');
+};
+
+// פתיחת תפריט ניווט ראשי
+let openMenue = document.querySelector('.right_nav_block');
+openMenue.addEventListener('click', openMenueFunction);
+
+function openMenueFunction (event){
+    let navigation_menu = document.querySelector('.navigation_menu');
+    navigation_menu.classList.add('navigation_menu_open_click');
+    navigation_menu.classList.remove('navigation_menu_close_click');
+};
+
+// סגירת תפריט ניווט ראשי בעת לחיצה על קישור
+let switchPageCloseMenu = document.querySelectorAll('.navigation_menu a');
+switchPageCloseMenu.forEach(item => {
+    item.addEventListener('click', function(event){
+        event.preventDefault();
+
+        let navigation_menu = document.querySelector('.navigation_menu');
+        navigation_menu.classList.add('navigation_menu_close_click');
+        navigation_menu.classList.remove('navigation_menu_open_click');
+
+        setTimeout (() => {
+            window.location.href = item.getAttribute('href');
+        }, 300)
+
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
     const wrapper = document.querySelector(".subcategory_wrapper_box");
     const content = document.querySelector(".subcategory_3d_box");
